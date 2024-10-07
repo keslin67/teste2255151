80e1aa6522f9f5cc2bf87cf867ea88ce5d711c69
keslin67 <105889460+keslin67@users.noreply.github.com>
2024-10-07 19:23:22 -0300

    lkmjjn

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -5,4 +5,8 @@ async function quantidadeUsuarios() {
       
       console.log(dados)
   }
+  const grafico = document.createElement('div')
+  grafico.className = 'grafico'
+  document.getElementById('graficos-container').appendChild(grafico)
+  Plotly.newPlot(grafico, data)
   
